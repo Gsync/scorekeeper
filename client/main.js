396fc76bb08a51b13c65ae6924540011c84842bb
Khuram Niaz <knshaikh@gmail.com>
2017-12-29 15:37:33 -0700

    import react modules

--- client/main.js
+++ client/main.js
@@ -1,5 +1,12 @@
-// import './../imports/utils';
-import someDefault from './../imports/utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor'; //meteor modules in meteor director
 
-console.log('Log from /client/main.js');
-console.log(someDefault);
+Meteor.startup(function () {
+
+    let name = 'Khuram';
+    let jsx = <p>Hello {name}</p>;
+    ReactDOM.render(jsx, document.getElementById('app'));
+
+});
+
