68d11793c41a6f786211777364f41680cad97210
Khuram Niaz <knshaikh@gmail.com>
2017-12-30 00:12:54 -0700

    Add playerName form

--- client/main.js
+++ client/main.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor'; //meteor modules in meteor director
+import { Players } from './../imports/api/players';
+import {Tracker} from 'meteor/tracker';
 
-const players = [{
-    _id: '1',
-    name: 'Mike',
-    score: 98
-}, {
-    _id: '2',
-    name: 'Jack',
-    score: -1
-}, {
-    _id: '3',
-    name: 'Susane',
-    score: -12
-}];
+
 
 const renderPlayers = function (players) {
     return players.map(function (player) {
@@ -24,17 +14,37 @@ const renderPlayers = function (players) {
     });
 };
 
+const handleSubmit = function (e) {
+    e.preventDefault();
+    let playerName = e.target.playerName.value; //form input name property
+    if (playerName) {
+        e.target.playerName.value = '';
+        Players.insert({
+            name: playerName,
+            score: 15
+        });
+    }
+};
+
 Meteor.startup(function () {
-    let title = 'Score Keep';
-    let name = 'Khuram';
-    let jsx = (
-        <div>
-            <h1>{title}</h1>
-            <p>Hello {name}!</p>
-            {renderPlayers(players)}
-        </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+
+    Tracker.autorun(function () {
+        let players = Players.find().fetch();
+        let title = 'Score Keep';
+        let name = 'Khuram';
+        let jsx = (
+            <div>
+                <h1>{title}</h1>
+                <p>Hello {name}!</p>
+                {renderPlayers(players)}
+                <form action="POST" onSubmit={handleSubmit}>
+                    <input type="text" name="playerName" placeholder="Player Name..." />
+                    <button>Add Player</button>
+                </form>
+            </div>
+        );
+        ReactDOM.render(jsx, document.getElementById('app'));
+    });
 
 });
 
--- server/main.js
+++ server/main.js
@@ -3,9 +3,8 @@ import { Players } from './../imports/api/players';
 
 Meteor.startup(function () {
 
-    Players.insert({
-        name: 'Mike',
-        score: 5
-    });
-    console.log(Players.find().fetch());
+    // Players.insert({
+    //     name: 'Mike',
+    //     score: 5
+    // });
 });
