231b8c4d3c3e0b5a2a61391b1d92da77bd1e94fc
Khuram Niaz <knshaikh@gmail.com>
2017-12-30 20:51:23 -0700

    Title css style

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default class TitleBar extends React.Component {
     render() {
         return (
-            <div>
+            <div className="title-bar">
+                <div className="wrapper">
                 <h1>{this.props.title}</h1>
+                </div>
             </div>
         );
     }
